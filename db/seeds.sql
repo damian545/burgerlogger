d68ea3cb27b3ded7553a4073363fe352bc21c666
Damian Tapp <dam180@hotmail.com>
2020-05-24 13:59:13 +1000

    added

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+insert into burgers_db
+    (burger_name)
+values
+    ('Cheese Burger');
+insert into burgers_db
+    (burger_name)
+values
+    ('Hamburger');
+insert into burgers_db
+    (burger_name)
+values
+    ('Bacon Cheese Burger');
+insert into burgers_db
+    (burger_name)
+values
+    ('Garlic Burger');
